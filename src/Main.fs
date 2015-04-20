16c7ccc898cadfb0c467b9d5a0ec6c5f3e33b893
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2015-04-20 19:44:52 -0400

    Initial Commit

---
+++ src/Main.fs
@@ -0,0 +1,25 @@
+open System
+
+open Suave
+open Suave.Web
+open Suave.Http
+open Suave.Http.Applicatives
+open Suave.Http.Files
+open Suave.Http.Successful
+open Suave.Types
+open Suave.Log
+open System.IO
+open System.Text
+
+//let logger = Loggers.sane_defaults_for Debug
+
+let app =
+  choose
+    [ GET >>= choose
+        [ path "/hello" >>= OK "Hello GET"
+          path "/goodbye" >>= OK "Good bye GET" ]
+      POST >>= choose
+        [ path "/hello" >>= OK "Hello POST"
+          path "/goodbye" >>= OK "Good bye POST" ] ]
+
+startWebServer defaultConfig app
