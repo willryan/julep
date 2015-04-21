f4fa868ee3d10c1e426fce68001bcbd74353f39d
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2015-04-20 20:32:27 -0400

    update dependencies to include NUnit runner, working on geting canopy task

---
+++ default.fsx
@@ -0,0 +1,34 @@
+#r @"./packages/FAKE/tools/FakeLib.dll"
+
+open Fake
+
+Target "build" (fun _ ->
+  MSBuildDebug "bin" "Build" [ "Julep.fsproj" ]
+  |> Log "TestBuild-Output: "
+)
+
+
+Target "test" (fun _ -> 
+  let testDlls = !! ("test/bin/Debug/*Tests.dll")
+  testDlls 
+  |> NUnit (fun p -> 
+    {p with
+       DisableShadowCopy = true; 
+       OutputFile = "test/TestResults.xml"})
+)
+
+Target "spec" (fun _ -> 
+  // can't find file for some reason, or a mono issue
+  let canopyExe = "spec/bin/Debug/Julep.Specs.exe"
+  let result = 
+    ExecProcess (fun info ->
+      info.FileName <- canopyExe
+      info.WorkingDirectory <- "spec"
+    ) (System.TimeSpan.FromMinutes 5.)
+
+  //ProcessHelper.killProcessById webSiteProcess.Id
+
+  if result <> 0 then failwith "Failed result from canopy tests"
+)
+
+RunTargetOrDefault "build"
