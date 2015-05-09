0fac766f7490adba097fafc746eedacf685ad0de
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2015-05-09 10:58:39 -0400

    attempt to find halfway solution for vs

--- src/CsvTest.fs
+++ src/CsvTest.fs
@@ -5,4 +5,4 @@ open FSharp.Data
 
 //type CsvTransactions = CsvProvider<"/Users/krelian18/Downloads/transactions_test.csv">
 
-
+//let txs = CsvTransactions.Load("/Users/krelian18/Downloads/transactions_test.csv")
